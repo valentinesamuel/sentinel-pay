db303decd88c2f77846b31999ea9a99d7ed489e4
valentinesamuel <valentinesamuel2580@gmail.com>
2025-11-13 18:35:39 +0100

    chore: added commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [
-            2,
-            'always',
-            ['feat', 'fix', 'docs', 'chore', 'refactor', 'ci', 'test', 'revert', 'perf'],
-        ],
-    },
-};
+export default { extends: ['@commitlint/config-conventional'] };
